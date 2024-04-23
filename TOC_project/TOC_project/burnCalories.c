457bbc1f6b0bfbfee7301abf57e72fad8f508091
shyeon0602 <july8560@naver.com>
2024-04-24 00:50:10 +0900

    modify all files -> add printExercise function

--- TOC_project/TOC_project/burnCalories.c
+++ TOC_project/TOC_project/burnCalories.c
@@ -3,11 +3,19 @@
 
 extern struct Exercise exerciseList[];
 
+int totalKcal = 1230;
+
+int calculateExerciseTime(int index) {
+	// 분 단위로 계산됨
+	int result = ((totalKcal / exerciseList[index].burnKcal) + 1) * 60;
+	return result;
+}
+
 int printExercise() {
-	printf("%s: %d Kcal", exerciseList[0].sportsName, exerciseList[0].burnKcal);
-
-
-
-
+	int i = 0;
+	for (int i = 0; i < numExercises; i++) {
+		int time = calculateExerciseTime(i);
+		printf("%s를 %d분동안 하면 칼로리를 태울 수 있습니다.\n", exerciseList[i].sportsName, time);
+	}
 	return 0;
 }
--- TOC_project/TOC_project/exerciseList.c
+++ TOC_project/TOC_project/exerciseList.c
@@ -3,4 +3,7 @@
 struct Exercise exerciseList[] = {
 	{"달리기", 60, 200},
 	{"줄넘기", 40, 220},
+	{"실내자전거", 25, 150},
 };
+
+numExercises = sizeof(exerciseList) / sizeof(exerciseList[0]);
