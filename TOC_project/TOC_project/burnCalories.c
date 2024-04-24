3528a4e55d71606fac4a08d88ae217819580cc62
shyeon0602 <july8560@naver.com>
2024-04-24 15:52:19 +0900

    modify burnCalories.c file -> modify printExercise function

--- TOC_project/TOC_project/burnCalories.c
+++ TOC_project/TOC_project/burnCalories.c
@@ -24,19 +24,33 @@ int calculateExerciseTimeMinute(int index) {
 int random() {
 	// 0~9 난수 생성
 	srand(time(NULL));
+	//srand(clock());		// pc 시작시간을 기준으로 하기 때문에 동일한 수 생성됨
 	int random = 0;
 	random = rand() % 9;
-	return random;
+	return random + 1;
 }
 
 int printExercise() {
 	int randomNum = random();
-	int i = 0;
+	int randomIndexArr[3] = { 0 };
 
-	for (i; i < numExercises; i++) {
-		int hour = calculateExerciseTimeHour(i);
-		int minute = calculateExerciseTimeMinute(i);
-		printf("%s를 %d시간 %d분 이상 하면 칼로리를 태울 수 있습니다.\n", exerciseList[i].sportsName, hour, minute);
+	for (int i = 0; i < 3; i++)
+	{
+		int randomIndex;
+		do
+		{
+			randomIndex = random() % numExercises;
+		} while (randomIndexArr[0] == randomIndex || randomIndexArr[1] == randomIndex || randomIndexArr[2] == randomIndex);
+
+		randomIndexArr[i] = randomIndex;
 	}
+
+	for (int j = 0; j < 3; j++)
+	{
+		int hour = calculateExerciseTimeHour(randomIndexArr[j]);
+		int minute = calculateExerciseTimeMinute(randomIndexArr[j]);
+		printf("%s를 %d시간 %d분 이상 하면 칼로리를 태울 수 있습니다.\n", exerciseList[randomIndexArr[j]].sportsName, hour, minute);
+	}
+
 	return 0;
 }
