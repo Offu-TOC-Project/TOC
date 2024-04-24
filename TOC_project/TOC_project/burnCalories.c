b6be2d5093af4701d9666569b4ac76545fd0970b
shyeon0602 <july8560@naver.com>
2024-04-24 12:20:35 +0900

    modify burnCalories.c file -> separate time exerciseTime function

--- TOC_project/TOC_project/burnCalories.c
+++ TOC_project/TOC_project/burnCalories.c
@@ -5,17 +5,25 @@ extern struct Exercise exerciseList[];
 
 int totalKcal = 1230;
 
-int calculateExerciseTime(int index) {
-	// 분 단위로 계산됨
-	int result = ((totalKcal / exerciseList[index].burnKcal) + 1) * 60;
-	return result;
+int calculateExerciseTimeHour(int index) {
+	int hour = totalKcal / exerciseList[index].burnKcal;
+	return hour;
+}
+
+int calculateExerciseTimeMinute(int index) {
+	int hour = totalKcal / exerciseList[index].burnKcal;
+	int restKcal = totalKcal - hour;
+	int caloriesPerMinute = exerciseList[index].burnKcal / exerciseList[index].minute;
+	int minute = (restKcal / caloriesPerMinute) % 60;
+	return minute;
 }
 
 int printExercise() {
 	int i = 0;
 	for (int i = 0; i < numExercises; i++) {
-		int time = calculateExerciseTime(i);
-		printf("%s를 %d분동안 하면 칼로리를 태울 수 있습니다.\n", exerciseList[i].sportsName, time);
+		int hour = calculateExerciseTimeHour(i);
+		int minute = calculateExerciseTimeMinute(i);
+		printf("%s를 %d시간 %d분 이상 하면 칼로리를 태울 수 있습니다.\n", exerciseList[i].sportsName, hour, minute);
 	}
 	return 0;
 }
