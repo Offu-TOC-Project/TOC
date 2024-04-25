bfee3bb8cee11d7a917eb55efdd49b3c0126b01a
shyeon0602 <july8560@naver.com>
2024-04-25 11:37:47 +0900

    merge Heeeee branch jeonsiyu branch

---
+++ TOC_project/TOC_project/FoodRecord.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <string.h>
+#include <Windows.h>
+
+#include "GOTOXY.h""
+
+#define SIZE 20
+
+
+typedef struct {
+	char FoodName[20];
+	int Gram;
+	int Kcal;
+}Food;
+
+Food FoodList[] = {
+	{"餌婁", 290, 53},
+	{"夥釭釭", 110,  79},
+	{"寡", 375, 26},
+	{"詬煩", 100, 38},
+	{"熱夢", 215, 31},
+	{"僻萄嬪纂", 190, 421},
+	{"僵楝萄", 150, 229},
+	{"殼陛蝙髦", 280, 170},
+	{"ABC輿蝶", 100, 32},
+	{"衛葬橡", 55, 383},
+	{"嫣", 210, 140},
+	{"絲梱蝶", 290, 268},
+	{"雇犖檜", 430, 173},
+	{"蘋溯", 200, 379},
+	{"盪渡瞼濰", 100, 90},
+	{"纂鑑", 650, 1649},
+	{"螃お塵", 45, 362},
+	{"塭賊", 550, 526},
+};
+
+char SaveName[SIZE][20];
+int SaveGram[SIZE];
+int SaveKcal[SIZE];
+
+
+int TotalKcal() {
+
+	int TotalKcal = 0;
+	
+	for (int i = 0; i < SIZE; i++){
+		for (int j = 0; j < sizeof(FoodList)/sizeof(FoodList[0]); j++){
+			if (strcmp(SaveName[i], FoodList[j].FoodName) == 0 && SaveGram[i] == FoodList[j].Gram) {
+				GotoXY(1, 7);
+				printf("%s %d g曖 蠔煎葬朝 %d kcal撿.\n", FoodList[j].FoodName, FoodList[j].Gram, FoodList[j].Kcal);
+								
+				TotalKcal += FoodList[j].Kcal;
+
+				GotoXY(1, 10);
+				KcalGraph(TotalKcal);						// 斜楚Щ 轎溘
+				
+				SaveKcal[i] = FoodList[j].Kcal;
+				GotoXY(40, i + 2);
+				printf("%s    %d    %d\n", SaveName[i], SaveGram[i], FoodList[j].Kcal);			// 殮溘脹 衝欽 援瞳 晦煙
+			}				
+		}
+	}
+	return TotalKcal;
+}
+
+int FoodRecord() {
+
+	int i = 0;
+	while(1){
+		printf("忙式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式忖\n\n");
+		printf("     螃棺 詳擎 擠衝 :                      ");
+		printf("\n\n     擠衝 曄 : ___ g                 ");
+		printf("\n\n戌式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式戎\n");
+
+		GotoXY(22, 2);
+		scanf(" %s", SaveName[i]);
+		GotoXY(15, 4);
+		scanf(" %d", &SaveGram[i]);
+		i++;
+		getchar();
+
+		TotalKcal();
+				
+		if (menuChoice() == 20) {
+			break;
+		}
+
+		system("cls");
+		
+	} 
+
+	system("cls");
+}
---
+++ TOC_project/TOC_project/KcalGraph.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <time.h>
+#include <Windows.h>
+
+#include "GOTOXY.h""
+
+int Gragh_Morning(int TotalKcal) {          // 아침 메시지 출력
+    if (TotalKcal <= 450) {
+        printf("적당히 먹은 거 같아!\n\n");             // 대화 앞에 name 추가 예정
+    }
+    else if (TotalKcal <= 1050) {
+        printf("다음 식단을 조절해야겠어!\n\n");
+    }
+    else if (TotalKcal <= 1500) {
+        printf("너무 많이 먹은 거 같아 ㅠㅠ\n\n");
+    }
+    else {
+        printf("시작부터 너무 한 거 아냐?    \n\n");
+    }
+    
+}
+
+int Gragh_Lunch(int TotalKcal) {            // 점심 메시지 출력
+    if (TotalKcal <= 450) {
+        printf("더 먹어도 괜찮아!\n\n");
+    }
+    else if (TotalKcal <= 1050) {
+        printf("적당히 좋아 !!      \n\n");
+    }
+    else if (TotalKcal <= 1500) {
+        printf("너무 많이 먹은 거 같아 ㅠㅠ\n\n");
+    }
+    else {
+        printf("배가 터질거 같아. 저녁에 어쩔려구 그래???!?!?!??\n\n");
+    }
+    
+}
+
+int Gragh_Dinner(int TotalKcal) {           // 저녁 메시지 출력
+    if (TotalKcal <= 450) {
+        printf("아직 부족해. 너무 안 먹은거 아냐??\n\n");
+    }
+    else if (TotalKcal <= 1050) {
+        printf("조금 더 먹어도 될 거 같애         \n\n");
+    }
+    else if (TotalKcal <= 1500) {
+        printf("딱 좋아.                          \n\n");
+    }
+    else {
+        printf("너무 많이 먹은거 같아 ㅠㅠ        \n\n");
+    }
+    
+}
+
+int KcalGraph(int TotalKcal) {
+    
+
+    time_t t;
+    struct tm* localTime;
+
+                                                // 입력된 kcal가 누적된 하루 적정 kcal 그래프
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 6);
+    GotoXY(0, 9);
+    printf("[");
+    for (int i = 0; i < 24; ++i) {
+        if (i * 1500 / 24 <= TotalKcal) {
+            printf("■");                                                // 누적 합계 그래프 표시
+        }
+        else {
+            printf(" ");
+        }
+    }
+    printf("%d%%]", TotalKcal * 100 / 1500);
+
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 8);
+    GotoXY(10, 10);
+    printf("%d /1500 Kcal\n\n", TotalKcal);                             // 누적 합계 숫자로 표시
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+
+
+    time(&t); // 현재 시간을 초 단위로 얻음
+    localTime = localtime(&t); // 초 단위의 시간을 현지 시간대로 변환
+
+    if (3 <= localTime->tm_hour && localTime->tm_hour <= 9) {           // 시간에 맞춰 메세지 출력
+        Gragh_Morning(TotalKcal);
+    }
+    else if (9 < localTime->tm_hour && localTime->tm_hour <= 15) {
+        Gragh_Lunch(TotalKcal);
+    }
+    else if (15 < localTime->tm_hour && localTime->tm_hour <= 24) {
+        Gragh_Dinner(TotalKcal);
+    }
+
+
+}
