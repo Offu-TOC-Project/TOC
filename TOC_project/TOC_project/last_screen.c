b5047f2b6c6f7f6b090c9f47b7fab86b9a132c0e
jeonsiyu <dkfker1010@gmail.com>
2024-04-26 09:51:26 +0900

    last_screen 추가

---
+++ TOC_project/TOC_project/last_screen.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <Windows.h>
+#include "Cursor.h"
+#define UP 1
+#define DOWN 2
+#define SPACE 3
+
+
+int last_screen() {
+	//	칼로리 합계로 인한 성공 / 실패 화면 함수
+		if (Totalkcal < presonalKcal()) {
+			prints_ch2();
+			gotoxy(22, 25);
+			printf("오늘의 식단관리 성공!!\n");
+			gotoxy(22, 26);
+			printf("너무 좋은데? 내일도 와줘야해ㅎㅎ\n");
+		}
+		else {
+			prints_ch4();
+			gotoxy(22, 25);
+			printf("오늘의 식단관리 실패ㅜ.ㅜ\n");
+			gotoxy(22, 26);
+			printf("괜찮아ㅜ.ㅜ 그럴 수 있지.. 내일은 더 잘 해보자!\n");
+		}
+	}
+}
